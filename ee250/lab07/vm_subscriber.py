d0b7c02a78e8a3a5224f55ff83808ac01e84cdaa
Andrew Zhou <andrewzh@usc.edu>
2018-03-20 17:09:49 -0700

    fixed indenting error

--- ee250/lab07/vm_subscriber.py
+++ ee250/lab07/vm_subscriber.py
@@ -5,19 +5,22 @@ Run vm_subscriber.py in a separate terminal on your VM."""
 import paho.mqtt.client as mqtt
 import time
 
-def led_connect(client, userdata, flags):
 	
-
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
-
+    client.subscribe("anrg-pi13/ultrasonicRanger")
     #subscribe to the ultrasonic ranger topic here
-    client.subscribe("anrgpi-13/ultrasonic_Ranger")
+    client.message_callback_add("anrg-pi13/ultraSonicRanger", ultraSonic_callback)
+
+
+
+def ultraSonic_callback(client, userdata, message):
+        print("Ultrasonic Read: " + str(message.payload))
+
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload))
 
-def on_press(key):
 if __name__ == '__main__':
     #this section is covered in publisher_and_subscriber_example.py
     client = mqtt.Client()
